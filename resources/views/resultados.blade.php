ddd8b3a219f51838adbf71f97f6de7592a508e84
CruzAlumno <118339822+CruzAlumno@users.noreply.github.com>
2023-06-14 10:44:21 +0200

    Update resultados.blade.php

--- resources/views/resultados.blade.php
+++ resources/views/resultados.blade.php
@@ -17,27 +17,37 @@
                                         <div class="image-wrapper">
                                             <div class="aspect">
                                                 <img
-                                                        src="{{ $equipo->imagen }}"
+                                                        src="https://lumiere-a.akamaihd.net/v1/images/Star-Wars-Phantom-Menace-I-Poster_f5832812.jpeg?region=0%2C250%2C678%2C340&width=600"
                                                         class="thumb reserved-ratio">
                                             </div>
                                         </div>
-                                        <div class="content-wrapper   ">
+                                        <div class="content-wrapper">
                                             <div class="bedazzlement"></div>
                                             <div class="content-bumper">
                                                 <div class="content-info">
                                                     <h3 class="title">
-                                                            <span class="long-title">{{ $equipo->premio }}</span>
+                                                            <span class="long-title">{{ $equipo['nombreEquipo'] }}</span>
                                                     </h3>
                                                     <div class="desc-sizer">
-                                                            <p class="desc">{{ $equipo->nombreCentro }}</p>
+                                                            <p class="desc">{{ $equipo['nombreCentro'] }}</p>
                                                     </div>
-                                                    @foreach( $equipo->miembros as $participante )
+
+                                                    <h4>Puntuaciones</h4>
+                                                    @foreach( $equipo['pruebas'] as $prueba )
                                                     <div class="anchored-text">
-                                                        <a href="/films/1/characters">
                                                         <h4 class="category-info"> <span
                                                                 class="content-icon films-icon"></span> <span
-                                                                class="category-name"></span>{{ $participante->nombre }}</h4>
-    
+                                                                class="category-name">{{ $prueba["nombre"] }}</span> {{ $prueba["puntuacion"] }}</h4>
+
+                                                    </div>
+                                                    @endforeach
+                                                    <h4>Miembros</h4>
+                                                    @foreach( $equipo['participantes'] as $participante )
+                                                    <div class="anchored-text">
+                                                        <h4 class="category-info">
+                                                            <span class="content-icon films-icon"></span>
+                                                            <span class="category-name">{{ $participante }}</span></h4>
+
                                                     </div>
                                                     @endforeach
                                                 </div>
@@ -45,10 +55,11 @@
                                             <div
                                                 class="metadata    mob-name mob-photo mob-date tab-name tab-photo tab-date desktop-name desktop-photo desktop-date   ">
                                                 <div class="anchored-text">
+                                                    <a href="/equipo/{{ $equipo['nombreEquipo'] }}">
                                                     <h4 class="category-info"> <span
                                                             class="content-icon films-icon"></span> <span
-                                                            class="category-name">{{ $equipo->nombreEquipo }}</span>
-                                                    </h4>
+                                                            class="category-name">Participantes</span> </h4>
+                                                    </a>
                                                 </div>
                                             </div>
                                         </div>
