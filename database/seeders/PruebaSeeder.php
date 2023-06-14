d3e595873e4134dc12cabeef3212599368fef57f
CruzAlumno <118339822+CruzAlumno@users.noreply.github.com>
2023-06-14 14:09:43 +0200

    Update PruebaSeeder.php

--- database/seeders/PruebaSeeder.php
+++ database/seeders/PruebaSeeder.php
@@ -15,18 +15,20 @@ class PruebaSeeder extends Seeder
     public function run()
     {
         DB::table('pruebas')->truncate();
-        foreach (self::$equipos as $equipo) {
+        foreach (self::$pruebas as $prueba) {
             DB::table('pruebas')->insert([
-                'nombre' => $equipo[0],
-                'descripcion' => $equipo[1],
-                'grado' => $equipo[2],
-                'id_olimpiada' => $equipo[3]
+                'nombre' => $prueba[0],
+                'descripcion' => $prueba[1],
+                'grado' => $prueba[2],
+                'id_olimpiada' => $prueba[3]
             ]);
         }
     }
 
-    private static $equipos = array(['Bases de datos', 'Debes de basear datos', 'superior', 1],
-                                    ['Haxxzor time', 'HAX HAX HAX HAX HAX HAX HAX HAX', 'modding', 2],
-                                    ['Bases de datos', 'Debes de basear MORE MORE datos', 'medio', 1],
-                                    ['Death Battle', 'we shouldnt do this', 'medio', 2]);
+    private static $pruebas = array(['superior 1', 'Debes de basear datos', 'superior', 1],
+                                    ['superior 2', 'HAX HAX HAX HAX HAX HAX HAX HAX', 'superior', 1],
+                                    ['superior 3', 'Debes de basear MORE MORE datos', 'superior', 1],
+                                    ['modding 1', 'we shouldnt do this', 'modding', 1]
+                                   ['modding 2', 'we shouldnt do this', 'modding', 1]
+                                   ['modding 3', 'we shouldnt do this', 'modding', 1]);
 }
