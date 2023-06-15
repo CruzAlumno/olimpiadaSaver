dd89804446501c693a3cfd7f6fdaef137330e020
CruzAlumno <118339822+CruzAlumno@users.noreply.github.com>
2023-06-15 13:24:23 +0200

    Update EquiposController.php

--- app/Http/Controllers/EquiposController.php
+++ app/Http/Controllers/EquiposController.php
@@ -127,7 +127,7 @@ class EquiposController extends Controller
 
     public function getEquipoForm($olimpiada){
 
-        return view('equipoFormulario', array('olimpiada' => $olimpiada));
+        return view('equipoFormulario', array('olimpiada' => $olimpiada, 'created' => false));
     }
 
     public function createEquipo(Request $request){
