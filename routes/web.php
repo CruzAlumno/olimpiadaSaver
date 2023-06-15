54944a39942fde3ab4d2188619456261b8b0dfb9
CruzAlumno <118339822+CruzAlumno@users.noreply.github.com>
2023-06-15 09:06:53 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,28 +5,19 @@ use App\Http\Controllers\EquiposController;
 use App\Http\Controllers\OlimpiadasController;
 use App\Http\Controllers\PruebasController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', [OlimpiadasController::class, 'getOlimpiadas']);
 Route::get('/admin/newOlimpiada', function () {
     return view('olimpiadaFormulario');
 });
 Route::post('/admin/newOlimpiada', [OlimpiadasController::class, 'createOlimpiada']);
 
-Route::get('/admin/pruebas', [PruebasController::class, 'getPruebas']);
-Route::get('/admin/newPrueba', [PruebasController::class, 'getPruebaForm']);
-Route::post('/admin/newPrueba', [PruebasController::class, 'createPrueba']);
+Route::get('/{olimpiada}/{grado}/pruebas', [PruebasController::class, 'getPruebas']);
+Route::get('/admin/{olimpiada}/{grado}/pruebas', [PruebasController::class, 'getPruebasAdmin']);
+Route::get('/admin/{olimpiada}/newPrueba', [PruebasController::class, 'getPruebaForm']);
+Route::post('/admin/{olimpiada}/newPrueba', [PruebasController::class, 'createPrueba']);
+Route::get('/admin/delete/prueba/{prueba}', [PruebasController::class, 'deletePrueba']);
 
-Route::get('/admin/newTeam', [EquiposController::class, 'getEquipoForm']);
+Route::get('/{olimpiada}/subscripcion', [EquiposController::class, 'getEquipoForm']);
 Route::get('/admin/confirm/{equipo}', [EquiposController::class, 'confirmEquipo']);
 Route::get('/admin/delete/equipo/{equipo}', [EquiposController::class, 'deleteEquipo']);
 Route::post('/admin/{olimpiada}/{grado}/equipos', [EquiposController::class, 'changeEquipoScore']);
@@ -34,6 +25,3 @@ Route::post('/admin/newTeam', [EquiposController::class, 'createEquipo']);
 Route::get('/{olimpiada}/{grado}/equipos', [EquiposController::class, 'getEquipos']);
 Route::get('/admin/{olimpiada}/{grado}/equipos', [EquiposController::class, 'getEquiposAdmin']);
 Route::get('/{olimpiada}/{grado}/resultados', [EquiposController::class, 'getResultados']);
-
-// Route::get('/equipo/{id}', [ParticipantesEquiposController::class, 'getParticipantesEquipo']);
-// Route::get('/resultados/{grado}', [EquiposController::class, 'getResultados']);
