e317f593115dac2ccd152b8e97b5fdd1112d987f
CruzAlumno <118339822+CruzAlumno@users.noreply.github.com>
2023-06-16 10:27:51 +0200

    Update secondNavbar.blade.php

--- resources/views/includes/secondNavbar.blade.php
+++ resources/views/includes/secondNavbar.blade.php
@@ -1,1 +1,23 @@
+<div id="secondNavbar">
+    <a class="secondLink" href="{{$admin}}/{{$ol}}/{{$gr}}/pruebas">Pruebas</a>
+    <a class="secondLink" href="{{$admin}}/{{$ol}}/{{$gr}}/equipos">Equipos</a>
+    <a class="secondLink" href="/{{$ol}}/{{$gr}}/resultados">Resultados</a>
+</div>
 
+<style>
+    #secondNavbar {
+        background-color: black;
+        color: white;
+        display: flex;
+        justify-content: safe center;
+        border: 5px solid white;
+    }
+
+    #secondNavbar .secondLink:link,
+    #secondNavbar .secondLink:visited,
+    #secondNavbar .secondLink:hover,
+    #secondNavbar .secondLink:active{
+        text-decoration: none;
+        color: white;
+    }
+</style>
